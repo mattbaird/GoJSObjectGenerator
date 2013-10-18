9ab7894481f59cbb9e4716def60c3e0a8ae38524
Matthew Baird <mattbaird@gmail.com>
2013-10-18 11:38:35 -0700

    Add simple test for file generation.

--- jsify_test.go
+++ jsify_test.go
@@ -1,16 +1,33 @@
 package jsify
 
 import (
+	"os"
 	"testing"
 )
 
-func Test_Example(t *testing.T) {
+func Test_NoErrors(t *testing.T) {
 	structs := make([]interface{}, 3)
 	structs[0] = &Foo{}
 	structs[1] = &Bar{}
 	structs[2] = &Baz{}
 	_, err := GenerateJavascriptToString(structs)
 	if err != nil {
+		t.Fail()
+	}
+}
+
+func Test_MakeFile(t *testing.T) {
+	fileName := "./structs.js"
+	defer os.Remove(fileName)
+	structs := make([]interface{}, 3)
+	structs[0] = &Foo{}
+	structs[1] = &Bar{}
+	structs[2] = &Baz{}
+	err := GenerateJavascriptToFile(fileName, structs)
+	if err != nil {
+		t.Fail()
+	}
+	if _, err := os.Stat(fileName); os.IsNotExist(err) {
 		t.Fail()
 	}
 }
